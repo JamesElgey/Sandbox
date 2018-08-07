297e2c7372dad45895642e02924e52118aeed7b7
JamesElgey <james.elgey@my.jcu.edu.au>
2018-08-07 18:12:38 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,1 +1,9 @@
 ### James Elgey
+MIN_LENGHT = 5
+
+password = input("Enter a password:")
+while len(password) < MIN_LENGHT:
+    print("Error, password too short...")
+    password = input("Enter a password:")
+print("Password accepted")
+print("*" * len(password))
